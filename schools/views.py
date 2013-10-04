f6f3bdb2e3900fddfc5b07bf55abf2f6b4c4add1
Cole Calhoun <calhouncole@gmail.com>
2013-10-04 10:08:15 -0700

    Allows user to signup and be added to the database

--- schools/views.py
+++ schools/views.py
@@ -17,28 +17,32 @@ class Intro(View):
 	    return render(request, 'schools/intro.html')
 
 	def post(self, request):
-		answer = request.POST.get("email")
+		email = request.POST.get("email")
 		password = request.POST.get("password")
 		password2 = request.POST.get("password2")
 		name = request.POST.get("name")
-		count = answer.find('@') 
-		answer = answer[count+1 : ]
+		count = email.find('@') 
+		email = email[count+1 : ]
 		user_database = Users.objects.filter(name = name).exists()
-		if answer == 'scu.edu':
+		if email == 'scu.edu':
 			if password == password2:
 				if user_database:
 					user_database = Users.objects.get(name = name)
 					request.session['id'] = user_database.id
 					return HttpResponseRedirect('/classifieds')
 				else:
-					return HttpResponse("hello")
+					new_usr = Users(name = name, email = email, school = Schools.objects.get(pk=1))
+					new_usr.save()
+					request.session['id'] = new_usr.id
+					return HttpResponseRedirect('/classifieds')
+
 			else:
 				return HttpResponse("Sorry, your passwords do not match")
 		else:
 			return HttpResponse("Sorry, that school is not in our database.")
 			#return HttpResponseRedirect('/classifieds', context)
 		
-			#new_usr = Users(name = name, email = answer, school = Schools.objects.get( pk = 1))
+			#new_usr = Users(name = name, email = email, school = Schools.objects.get( pk = 1))
 			#new_usr.save()
 			#return HttpResponse(new_usr.name)
 			
